333c538f1709fe2ba0bd548124f0cbb2a32706bb
Melih karakuş <melih.karakus1818@gmail.com>
2023-09-21 21:43:34 +0300

    Tasarımsal düzenlemeler

--- TravelsalProje/Controllers/CommentController.cs
+++ TravelsalProje/Controllers/CommentController.cs
@@ -1,11 +1,13 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 
 namespace TravelsalProje.Controllers
 {
+    [AllowAnonymous]
     public class CommentController : Controller
     {
         CommentManager commentManager = new CommentManager(new EfCommentDal()); //businesslayer içindeki concrete içindeki sınıftan miras alınıyor
--- TravelsalProje/Controllers/DefaultController.cs
+++ TravelsalProje/Controllers/DefaultController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TravelsalProje.Controllers
 {
     public class DefaultController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index() //ansayfayı başta oluşturmak için kullanılan yerdir default içindedir kendisi
         {
             return View();
